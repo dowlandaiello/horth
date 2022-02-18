14cbbd88f603e99047fbaa9355017060acff4485
Dowland Aiello <dowlandaiello@gmail.com>
2022-02-18 15:36:15 -0800

    fix compilation issues
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- main.hs
+++ main.hs
@@ -14,15 +14,16 @@ data CompilationError = CompilationError CompilationCtx String deriving (Show)
 
 -- Compile a list of tokens into assembly
 com :: [(String, CompilationCtx)] -> Either [CompilationError] String
+com [("", ctx)] = asm ("", ctx)
 com (x:xs) = case asm x of
-        Left e -> Left e:(fromLeft [] prog)
+        Left [e] -> Left $ e:(fromLeft [] prog)
         Right asm' -> case prog of
                 Left e -> Left e
-                Right asm'' -> Right asm':asm''
+                Right asm'' -> Right $ asm' ++ asm''
         where prog = com xs
 
 
-asm :: (String, CompilationCtx) -> Either CompilationError String
+asm :: (String, CompilationCtx) -> Either [CompilationError] String
 asm ("", _) = Right "mov $0x3c, %rax\n\
                     \mov $0, %rdi\n\
                     \syscall"
@@ -30,9 +31,9 @@ asm ("+", _) = Right "pop %rax\n\
                      \pop %rbx\n\
                      \add %rbx, %rax\n\
                      \push %rax\n"
-asm (tok, ctx) = "push $" ++ d ++ "\n"
-        where Just d = readMaybe tok
-asm (tok, ctx) = Left $ CompilationError ctx "invalid symbol `" ++ tok ++ "`"
+asm (tok, ctx)
+        | isJust (readMaybe tok :: Maybe Integer) = Right $ "push $" ++ tok ++ "\n"
+        | otherwise = Left $ [CompilationError ctx ("invalid symbol `" ++ tok ++ "`")]
 
 -- Tokenize immediate values
 tokenizeimm :: String -> [(String, CompilationCtx)]
